94dd38518f5e9054a21fc991c8510c60c74230aa
Santicabral0478 <scsantiagocabral@gmail.com>
2024-09-06 10:59:50 -0300

    update 2

--- src/components/AboutTwo/index.tsx
+++ src/components/AboutTwo/index.tsx
@@ -1,6 +1,41 @@
-import { FunctionComponent } from "react";
+"use client"
+import { IFormData } from "@/utils/IFormData";
+import { PostContactMail } from "@/utils/PostContactMail";
+import React, { FunctionComponent, useState } from "react";
 
 export const AboutTwo: FunctionComponent = ()=>{
+
+  const [contentForm, setContentForm] = useState<IFormData>({
+    fullName: "",
+    phone:  "",
+    gmail: "",
+    message:  "",
+  })
+
+  const [errors, setErrors] = useState("");
+
+  const handleOnSubmitForm = async (event: React.FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+  
+    if(errors){
+      return
+    }
+
+    try{
+      const responseForm = await PostContactMail(contentForm)
+      console.log(responseForm);
+    } catch(err){
+      console.log(err);
+      
+    }
+  }
+
+  const updateFormData = (prop: string, value: string)=> {
+    setContentForm(prevState => ({
+      ...prevState,
+      [prop]: value
+    }))
+  }
 
     return(
       <section className="section contact-form-cont has-bg-image pattern-element" aria-label="contact-form-cont">
@@ -15,12 +50,50 @@ export const AboutTwo: FunctionComponent = ()=>{
 
             <p className="section-subtitle has-before">Get every update</p>
             <br />
-            <form className="form">
+            <form onSubmit={handleOnSubmitForm} className="form">
                 <div className="title">Contact us</div>
-                <input type="text" placeholder="Your email" className="input"/>
-                <textarea placeholder="Your message"></textarea>
+                <input 
+                  name="fullName" 
+                  value={contentForm.fullName} 
+                  type="text" 
+                  placeholder="Nombre" 
+                  className="input"
+                  onChange={(e)=>{
+                    updateFormData("fullName", e.target.value)
+                  }}
+                />
+
+                <input 
+                  name="phone" 
+                  value={contentForm.phone} 
+                  type="phone" placeholder="Telefono" 
+                  className="input"
+                  onChange={(e)=>{
+                    updateFormData("phone", e.target.value)
+                  }}
+                />
+
+                <input 
+                  name="gmail" 
+                  value={contentForm.gmail} 
+                  type="email" 
+                  placeholder="Email" 
+                  className="input"
+                  onChange={(e)=>{
+                    updateFormData("gmail", e.target.value)
+                  }}
+                />
+
+                <textarea 
+                  name="message" 
+                  value={contentForm.message} 
+                  placeholder="Mensaje"
+                  onChange={(e)=>{
+                    updateFormData("message", e.target.value)
+                  }}
+                ></textarea>
                 
-                <button>Submit</button>
+                <button type="submit">Submit</button>
             </form>
 
           </div>
