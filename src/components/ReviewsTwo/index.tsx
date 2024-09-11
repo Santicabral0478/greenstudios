1dced2084c4dab7dde67d90bf443e64298172871
Santicabral0478 <scsantiagocabral@gmail.com>
2024-09-11 12:22:05 -0300

    Ckjfdbfupdate jhgvhgd

--- src/components/ReviewsTwo/index.tsx
+++ src/components/ReviewsTwo/index.tsx
@@ -18,7 +18,7 @@ export const ReviewsTwo = ()=>{
       id: 1,
       stars: 3,
       company: "apple",
-      review: "Lorem ipsum dolor sit amet consectetur ingt. vkjdfkd  moldgsdfglitia, molestiae quas vel sint commodi repudiandae consequuntur.",
+      review: "Lorem ipsum dolor sit amet consectetur ingt. vkjdfkd  moldgsdfglitia, molestiae quas jsbddjsht commodi repudiandae consequuntur.",
       img: "/about-1.png",
       position: "co funder",
     },
