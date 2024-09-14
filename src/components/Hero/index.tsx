7404bd79f338810402ba3c74fb8f1dd317d28237
Santicabral0478 <scsantiagocabral@gmail.com>
2024-09-13 23:28:32 -0300

    jhbcsj t55TER5656G

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -14,7 +14,7 @@ export const Hero: FunctionComponent = ()=>{
             </h1>
 
             <p className="hero-text">
-              Diseñamos y desarrollamos aplicaciones web de. Lleva tu jhsbhjd743843ZXXisl futuro p drgejrnada de    +iurssafío            
+              Diseñamos y desarrollamos aplicaciones web de. Lleva tu jhsbhjd743843ZXXisl futuro p stfhddtyh5 de +iurssafío            
             </p>
 
             <div className="wrapper">
