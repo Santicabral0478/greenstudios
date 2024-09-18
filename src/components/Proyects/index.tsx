b63d74788cf1388e9a1f722af74fae94d7a30251
Santicabral0478 <scsantiagocabral@gmail.com>
2024-09-17 22:49:00 -0300

    update 1-b bsjdhb d d

--- src/components/Proyects/index.tsx
+++ src/components/Proyects/index.tsx
@@ -72,7 +72,7 @@ export const Proyects: FunctionComponent = ()=>{
         <section className="section project" id="projects" aria-label="project">
         <div className="container project-section">
 
-          <p className="section-subtitle has-before text-center">Projectsdbdbdb</p>
+          <p className="section-subtitle has-before text-center">akjsdhfs</p>
 
           <h2 className="h2 section-title text-center">
             Green <span className="has-before">khdvbdfhb</span>
